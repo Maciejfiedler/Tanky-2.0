92c078d6e9ba1a83728b88f25d45e6c6b5a56847
Maciej Fiedler <maciekfiedler@gmail.com>
2021-05-08 19:47:45 +0200

    added w stream and python code

--- app.py
+++ app.py
@@ -1,17 +1,44 @@
 from flask import Flask, render_template
 from flask_socketio import SocketIO
+from threading import Timer
+from gpiozero import Robot
 
 app = Flask(__name__)
 socketio = SocketIO(app)
+robot = Robot(left=(7,8), right=(9,10))
+
 
 @app.route("/")
 def index():
     return render_template("index.html")
 
+
 @socketio.on('forward')
 def handle_forward(quantity):
-    print("forward")
-    print(quantity)
+    if (quantity != None):
+        try:
+            runCommandsInSteps('forward', quantity)
+        except:
+            pass
+
+
+def runCommandsInSteps(command, steps):
+    steps = int(quantity['steps'])
+    for x in range(steps):
+        pass
+        robot.forward(1)
+        t = Timer(0.2, robot.stop)
+        t.start()
+
+def runCommand(command):
+    if(command=='forward'):
+        robot.forward(1)
+    elif(command=='backward'):
+        robot.backward(1)
+
+    t = Timer(0.2,robot.stop)
+    t.start
+
 
 if '__main__' == __name__:
     socketio.run(app, host="0.0.0.0", debug=True)
